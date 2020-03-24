d3d25fd786b016cf9e0bf468c8f8e0fcea05ba10
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-03-25 00:10:02 +0600

    Registration backend code done with Firebase!

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,61 +1,113 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:traintracker/screens/dashboard_screen.dart';
 import 'package:traintracker/utils/constants.dart';
 import 'package:traintracker/utils/reuseable_widgets.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:toast/toast.dart';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  TextEditingController _emailController = TextEditingController();
+
+  TextEditingController _passwordController = TextEditingController();
+
+  bool isEnable = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('images/background.png'),
-                fit: BoxFit.cover
-            )
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 150,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              child: TextField(
-                textAlign: TextAlign.center,
-                decoration: kRoundedTextFieldDecoration.copyWith(
-                  hintText: 'Email'
+      body: ModalProgressHUD(
+        inAsyncCall: isEnable,
+        child: Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/background.png'),
+                  fit: BoxFit.cover
+              )
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 150,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: _emailController,
+                  textAlign: TextAlign.center,
+                  decoration: kRoundedTextFieldDecoration.copyWith(
+                    hintText: 'Email'
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              child: TextField(
-                textAlign: TextAlign.center,
-                obscureText: true,
-                decoration: kRoundedTextFieldDecoration.copyWith(
-                    hintText: 'Password',
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                child: TextField(
+                  controller: _passwordController,
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  decoration: kRoundedTextFieldDecoration.copyWith(
+                      hintText: 'Password',
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            RoundedCornerButton(
-              color: kMainColor,
-              text: 'Register',
-              textColor: Colors.white,
-              borderColor: Colors.white,
-              onPress: (){
-
-              },
-            ),
-          ],
+              SizedBox(
+                height: 15,
+              ),
+              RoundedCornerButton(
+                color: kMainColor,
+                text: 'Register',
+                textColor: Colors.white,
+                borderColor: Colors.white,
+                onPress: () async{
+                  setState(() {
+                    isEnable = true;
+                  });
+                  try{
+                    var _user = await _firebaseAuth.createUserWithEmailAndPassword(
+                        email: _emailController.text.trim(),
+                        password: _passwordController.text
+                    );
+                    if (_user != null){
+                      _emailController.clear();
+                      _passwordController.clear();
+                      Navigator.push(context, MaterialPageRoute(
+                        builder: (BuildContext context) => DashBoard()
+                      ));
+                    }
+                    setState(() {
+                      isEnable = false;
+                    });
+                  } catch (e) {
+                    setState(() {
+                      isEnable = false;
+                    });
+                    Toast.show(
+                        '${e.toString()}',
+                        context,
+                        textColor: Colors.white,
+                        backgroundColor: Colors.red,
+                      duration: Toast.LENGTH_LONG
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
