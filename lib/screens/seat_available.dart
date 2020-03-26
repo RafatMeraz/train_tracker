307b48ff77a89c7b2235cfe51c994a5ed1e69441
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-03-26 15:30:39 +0600

    seat available is done with circularprogressindecator

--- lib/screens/seat_available.dart
+++ lib/screens/seat_available.dart
@@ -6,7 +6,7 @@ import 'package:traintracker/utils/reuseable_widgets.dart';
 import 'package:traintracker/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SeatAvailable extends StatelessWidget {
   @override
@@ -18,20 +18,45 @@ class SeatAvailable extends StatelessWidget {
             'Seat Available'
         ),
       ),
-      body: ListView(
-        children: <Widget>[
-          TrainScheduleCard(
-            from: 'Dhaka',
-            to: 'Tangail',
-            time: 'AC : 17   NON-AC: 3',
-          ),
-          TrainScheduleCard(
-            from: 'Dhaka',
-            to: 'Sirajgonj',
-            time: 'AC : 1   NON-AC: 7',
-          ),
-        ],
-      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('seats').snapshots(),
+        builder: (context, snapshot){
+          if (snapshot.hasData){
+            var seats = snapshot.data.documents;
+            List<Card> allSeats = [];
+            for (var seat in seats){
+              final destination = seat.data['destination'];
+              final time = seat.data['time'];
+              final ac = seat.data['ac'];
+              final non_ac = seat.data['non-ac'];
+              final s = Card(
+                child: ListTile(
+                  leading: Icon(
+                    Icons.airline_seat_recline_extra,
+                    color: kMainColor,
+                  ),
+                  title: Text(
+                    '$destination'
+                  ),
+                  subtitle: Text(
+                      'AC: $ac  Non-AC: $non_ac'
+                  ),
+                  trailing: Text(
+                    '$time'
+                  ),
+                ),
+              );
+              allSeats.add(s);
+            }
+            return ListView(
+              children: allSeats,
+            );
+          }
+          return Center(
+              child: CircularProgressIndicator()
+          );
+        },
+      )
     );
   }
 }
